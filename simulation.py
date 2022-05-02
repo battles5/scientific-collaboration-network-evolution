8663e4cfd90ce714d0526b7c4f69222caa91dd32
Orso Peruzzi <orso.peruzzi@studio.unibo.it>
2022-05-02 10:05:08 +0200

    "initialize" and "observe" function coded. i'm not sure if they will work.

--- simulation.py
+++ simulation.py
@@ -13,8 +13,15 @@ m = 1  # Number of edges per new node
 
 # Main functions of the network simulation
 def initialize():
+    global g # Defining g graph (networkx class) as a global variable
+    g = nx.complete_graph(m0) # We start with a fully connected graph
+    g.pos = nx.spring_layout(g) # We want to draw the graph using the nx "spring" layout for better visualization
+    g.count = 0
 
 def observe():
+global g
+cla()  # Cleaning axes (maybe we had a previous batch and we want to clean the graph axes)
+nx.draw(g, pos=g.pos)  # Drawing the graph
 
 def pref_select(nds):
 
