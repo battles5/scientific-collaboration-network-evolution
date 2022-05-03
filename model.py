c512fda9c7f074619310284794f63a8d8793519d
Orso Peruzzi <orso.peruzzi@studio.unibo.it>
2022-05-03 10:39:58 +0200

    added graph simulation with plotting functions. now they plot in a single image, i want to change it in a subplot grid.

--- model.py
+++ model.py
@@ -5,6 +5,8 @@ Created on Mon May 02 14:47:23 2022
 """
 import functions as fn
 import configparser
+import networkx as nx
+from pylab import *
 
 DEFAULTS = "configuration.txt"
 config = configparser.ConfigParser()
@@ -15,11 +17,21 @@ beta = config.getint('settings', 'beta')
 b = config.getint('settings', 'b')
 N = config.getint('settings', 'N')
 
+g = nx.barabasi_albert_graph(N, 5)
 alpha = a/b
 t = N/beta
-N = list(range(1, int(beta * t + 1), int(beta)))
+nlist = list(range(1, int(beta * t + 1), int(beta)))
 
 y = fn.average_links_at_t(t, alpha, b)
-x = N
+x = nlist
 
 fn.plotting(x, y, 'N', '<k>')
+
+
+Pk = [float(j) / N for j in nx.degree_histogram(g)]
+domain = range(len(Pk))
+loglog(domain, Pk, ':', label='Barabasi-Albert')
+xlabel('$k$')
+ylabel('$P(k)$')
+legend()
+show()
