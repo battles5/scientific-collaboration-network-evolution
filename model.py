30ba1676dfae923979fec79fd8c604081582d061
Orso Peruzzi <orso.peruzzi@studio.unibo.it>
2022-05-03 10:09:08 +0200

    continuous model.py is now working with first plotting result [<k> as function of N].

--- model.py
+++ model.py
@@ -4,10 +4,22 @@ Created on Mon May 02 14:47:23 2022
 @author: Orso Peruzzi
 """
 import functions as fn
+import configparser
 
+DEFAULTS = "configuration.txt"
+config = configparser.ConfigParser()
+config.read(DEFAULTS)
+
+a = config.getfloat('settings', 'a')
+beta = config.getint('settings', 'beta')
+b = config.getint('settings', 'b')
+N = config.getint('settings', 'N')
+
+alpha = a/b
 t = N/beta
-N = list(range(1, beta * t + 1, beta))
+N = list(range(1, int(beta * t + 1), int(beta)))
 
-y = fn.average_links_at_t(t)
+y = fn.average_links_at_t(t, alpha, b)
 x = N
 
+fn.plotting(x, y, 'N', '<k>')
